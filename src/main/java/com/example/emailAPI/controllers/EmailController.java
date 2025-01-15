d518f82b4bb798820c02c09565ac8141e4e9a6c6
jesus <jesuslopj0@gmail.com>
2025-01-15 17:22:30 +0100

    Implementando funcionalidad update y delete

--- src/main/java/com/example/emailAPI/controllers/EmailController.java
+++ src/main/java/com/example/emailAPI/controllers/EmailController.java
@@ -3,6 +3,8 @@ package com.example.emailAPI.controllers;
 import com.example.emailAPI.models.Email;
 import com.example.emailAPI.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,8 +28,29 @@ public class EmailController {
     }
 
     @GetMapping("/email/{id}")
-    public Email getEmailByID(@PathVariable long id) {
+    public ResponseEntity<?> getEmailByID(@PathVariable long id) {
         Optional<Email> emailResponse = emailService.getEmailByID(id);
-        return emailResponse.get();
+        if (emailResponse.isPresent()) {
+            return ResponseEntity.ok(emailResponse.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Email no encontrado");
+        }
     }
+
+    @PutMapping("actualizarEmail/{id}")
+    public String updateEmail(@PathVariable long id, @RequestBody Email newEmail) {
+        emailService.updateEmail(id, newEmail);
+        return "Email con id: "+id+" actualizado correctamente.";
+    }
+
+    @DeleteMapping("borrarEmail/{id}")
+    public ResponseEntity<String> borrarEmail(@PathVariable long id) {
+        if (emailService.deleteEmail(id)){
+            return ResponseEntity.ok("Email con id: "+id+" eliminado correctamente.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se pudo encontrar el email con id: "+id);
+        }
+
+    }
+
 }
--- src/main/java/com/example/emailAPI/services/EmailService.java
+++ src/main/java/com/example/emailAPI/services/EmailService.java
@@ -22,4 +22,24 @@ public class EmailService {
     public Optional<Email> getEmailByID(long id) {
         return emailList.stream().filter(email -> email.getId() == id).findFirst();
     }
+
+    public void updateEmail(long id, Email newEmail) {
+        getEmailByID(id).ifPresent(email -> {
+            email.setAsunto(newEmail.getAsunto());
+            email.setCuerpo(newEmail.getCuerpo());
+            email.setRemitente(newEmail.getRemitente());
+            email.setDestinatario(newEmail.getDestinatario());
+            email.setFecha(newEmail.getFecha());
+        });
+    }
+
+    public boolean deleteEmail(long id) {
+        Optional<Email> email = getEmailByID(id);
+        if (email.isPresent()) {
+            emailList.remove(email.get());
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
